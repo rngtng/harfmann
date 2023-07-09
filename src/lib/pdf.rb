9dbf1525d145df03c943a3c2d94bdef66ee31274
Tobias Bielohlawek <Tobias.Bielohlawek@solarisbank.de>
2023-07-09 23:04:03 +0200

    Add more

--- src/lib/pdf.rb
+++ src/lib/pdf.rb
@@ -1,15 +1,9 @@
+# frozen_string_literal: true
+
 require 'prawn'
 require 'prawn/table'
 
 Prawn::Fonts::AFM.hide_m17n_warning = true
-
-RE_NUMBER = "Auftragsbestätigung-140-Lila Lämmchen GmbH"
-
-RECEIVER = <<~RECEIVER
-  Lila Lämmchen GmbH
-  Maybachufer 23
-  12047 Berlin
-RECEIVER
 
 class Pdf
   include Prawn::View
@@ -27,69 +21,67 @@ class Pdf
     font_families.update(
       'SuperGrotesk' => {
         normal: "./assets/fonts/SuperGrotesk.ttf",
-        bold: "./assets/fonts/SuperGrotesk-Bold.ttf",
+        bold: './assets/fonts/SuperGrotesk-Bold.ttf'
       }
-     )
-    add_header(RECEIVER)
+    )
   end
 
-  def add_header(receiver)
+  def add_header(title, receiver)
     move_up 10
-    image "./assets/logo.jpeg", width: 100
+    image './assets/logo.jpeg', width: 100
     move_up 10
     font 'SuperGrotesk'
     text(HEADER,
-      size: BASE_SIZE - 2,
-      leading: 2,
-    )
+         size: BASE_SIZE - 2,
+         leading: 2)
     move_down BASE_SIZE
 
     text(
       receiver,
       size: BASE_SIZE,
-      leading: 2,
+      leading: 2
     )
-    move_down 2*BASE_SIZE
+    move_down 2 * BASE_SIZE
 
     text(
-      Time.now.strftime("%d.%m.%Y"),
-      size: BASE_SIZE,
+      Time.now.strftime('%d.%m.%Y'),
+      size: BASE_SIZE
     )
     move_down BASE_SIZE
 
     text(
-      RE_NUMBER,
+      title,
       size: BASE_SIZE + 2,
-      style: :bold,
+      style: :bold
     )
-    move_down 3*BASE_SIZE
+    move_down 3 * BASE_SIZE
 
     text(
-      "Folgende Artikel werden geliefert:",
-      size: BASE_SIZE,
+      'Folgende Artikel werden geliefert:',
+      size: BASE_SIZE
     )
     move_down 10
     @lines += 10
   end
 
   def add_pagenumbers
-    number_pages("Seite <page>/<total>", {
-      at: [bounds.right - 150, 0],
-      size: BASE_SIZE - 2,
-      # width: 150,
-      align: :right,
-    })
+    number_pages('Seite <page>/<total>', {
+                   at: [bounds.right - 150, 0],
+                   size: BASE_SIZE - 2,
+                   # width: 150,
+                   align: :right
+                 })
   end
 
   def add_table(headings:, rows:)
     next_page if @lines + rows.size > MAX_LINES
     font 'Helvetica'
-    table([headings, *rows], :width => 540, :cell_style => { :size => BASE_SIZE - 4, :overflow => :shrink_to_fit },  :column_widths => {
-      0 => 100,
-      1 => 40,
-      -2 => 20,
-      -1 => 40,
-    }) do
+    table([headings, *rows], width: 540, cell_style: { size: BASE_SIZE - 4, overflow: :shrink_to_fit }, column_widths: {
+            0 => 100,
+            1 => 40,
+            -2 => 20,
+            -1 => 40
+          }) do
       cells.border_width = 0.5
       row(0).columns(0..-1).borders = [:bottom]
       row(0).columns(0..-1).border_width = 1
@@ -103,38 +95,35 @@ class Pdf
   def add_total_table(headings:)
     next_page if @lines + 1 > MAX_LINES
     font 'Helvetica'
-    table([headings], :width => 540, :cell_style => { :size => BASE_SIZE - 4, :overflow => :shrink_to_fit },  :column_widths => {
-      1 => 25,
-      2 => 40,
-    }) do
+    table([headings], width: 540, cell_style: { size: BASE_SIZE - 4, overflow: :shrink_to_fit }, column_widths: {
+            1 => 25,
+            2 => 40
+          }) do
       cells.border_width = 0.5
       row(0).columns(0..2).borders = [:top]
       row(0).columns(0..2).border_width = 1
       row(0).columns(0..2).align = :right
     end
     move_down 20
-    @lines +=  2
+    @lines += 2
   end
 
   def add_footer
     font 'SuperGrotesk'
     move_down 20
     text("Vielen Dank für ihren Einkauf bei harfmann piccolino\n\n",
-      size: BASE_SIZE,
-    )
+         size: BASE_SIZE)
     move_down 10
 
     text("Herzliche Grüße,\n harfmann piccolino",
-      size: BASE_SIZE,
-    )
+         size: BASE_SIZE)
 
     move_down 20
 
-    text("GLS Bank   <b>IBAN</b> DE2643 0609 6710 6376 5400   <b>BIC</b> GENODEM 1 GLS",
-      size: BASE_SIZE - 2,
-      inline_format: true,
-      align: :center,
-   )
+    text('GLS Bank   <b>IBAN</b> DE2643 0609 6710 6376 5400   <b>BIC</b> GENODEM 1 GLS',
+         size: BASE_SIZE - 2,
+         inline_format: true,
+         align: :center)
   end
 
   def render(filename)
--- src/main.rb
+++ src/main.rb
@@ -1,19 +1,32 @@
+#!/usr/bin/env ruby
 # frozen_string_literal: true
 
 require 'simple_xlsx_reader'
 require './lib/pdf'
 
 pdf = Pdf.new
-doc = SimpleXlsxReader.open('./assets/VO_HW23_harfmann.xlsx')
+sheet = SimpleXlsxReader.open(ARGV[0]).sheets.first
 
 def num(amount)
   return unless amount
+
   "#{amount}x"
 end
 
 def money(price)
-  sprintf('%.2f €', price).gsub('.', ',')
+  format('%.2f €', price).gsub('.', ',')
 end
+
+sheet.rows.slurp
+nr, name = sheet.rows[2][6].split(" - ")
+street, city = sheet.rows[4][6].split(", ")
+
+title = "Auftragsbestätigung-#{nr}-#{name}"
+pdf.add_header(title, <<~RECEIVER)
+  #{name}
+  #{street}
+  #{city}
+RECEIVER
 
 out = false
 order = {}
@@ -22,7 +35,7 @@ total = 0
 total_size = 0
 buckets = []
 table = []
-doc.sheets.first.rows.each do |row|
+sheet.rows.each do |row|
   if out
     product = row[1] unless row[1].nil?
     key = "#{product} - #{row[2]} - #{row[3]}"
@@ -30,7 +43,7 @@ doc.sheets.first.rows.each do |row|
     line_size = 0
     any = false
 
-    row[4..-8][0...4*buckets.size].each_slice(4) do |slice|
+    row[4..-8][0...4 * buckets.size].each_slice(4) do |slice|
       # next if slice[0].nil?
 
       order[key] ||= []
@@ -42,32 +55,32 @@ doc.sheets.first.rows.each do |row|
       total += slice[0].to_i * slice[1].to_f
       total_size += slice[0].to_i
     end
-    table << ["#{product}", row[3], *order[key].flatten, num(line_size), money(line_total)] if any
+    table << [product.to_s, row[3], *order[key].flatten, num(line_size), money(line_total)] if any
   elsif row[4] && row[5].nil? && row[8] && row[9].nil?
     buckets = []
     row[4..-8].each_slice(4) do |slice|
-      buckets << {content: slice[0], colspan: 2, align: :center} unless slice[0].nil?
+      buckets << { content: slice[0], colspan: 2, align: :center } unless slice[0].nil?
     end
   end
   out = true if row[1] == 'STYLE'
-  if row[1].nil? && row[2].nil?
-    if out
-      pdf.add_table(
-        headings: [ "Artikel", "Farbe", *buckets, "Anz", "Preis" ],
-        rows: table,
-      )
-      table = []
-    end
-    out = false
+  next unless row[1].nil? && row[2].nil?
+
+  if out && table.any?
+    pdf.add_table(
+      headings: ['Artikel', 'Farbe', *buckets, 'Anz', 'Preis'],
+      rows: table
+    )
+    table = []
   end
+  out = false
 end
 
 pdf.add_total_table(
   headings: [
-    { content: "<b>Gesamt:</b>", align: :right, inline_format: true },
+    { content: '<b>Gesamt:</b>', align: :right, inline_format: true },
     num(total_size),
     money(total)
-  ],
+  ]
 )
 
-pdf.render('/output/file.pdf')
+pdf.render("/output/#{title}.pdf")
